a6266de16b19822e1a022be034db17eaca3fb40c
Jack Farrell <farrellj@stu.otc.school.nz>
2023-08-03 13:40:56 +1200

    Resubmit of fixing pep8 issues
    This will make the current version more up to date

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+"""Digital Science Program 2023 - Jack Farrell"""
+
+from tkinter import Label, LabelFrame, Button, Tk
+
+
 # L2 DISC Program 2023: Drama Prep
 
 # green #7CDF64
@@ -9,48 +14,86 @@
 # purple #724E91
 # yellow #E8C04A
 
+# pylint: disable = E1111
 
-import tkinter as tk
 
-program = tk.Tk()
+program = Tk()
 program.attributes('-fullscreen', True)
 program.configure(bg="#E8C5D8")
-
 
 
 window = "opening window"
 
 
-
 def closeprogram():
+    """This function closes the program when run"""
     program.destroy()
 
-exit_button = tk.Button(program, command=lambda:closeprogram(), text="exit", padx=10, pady=2, bg="#F45866", fg="#131117", font=("Arial", 25)).place(x=1775, y=40)
+
+exit_button = Button(program, command=closeprogram, text="exit",
+                     padx=10, pady=2, bg="#F45866", fg="#131117", font=("Arial", 25))
+exit_button.place(x=1775, y=40)
 
 
 def open_window(new_window, old_window):
     global opening_frame
     global info_frame
 
+    if new_window == "opening window":
+        window = new_window
+
+        opening_frame = LabelFrame(
+            program, width=1720, height=900, bg="#E2B6CE")
+        opening_frame.pack(anchor="nw")
+
+        start_button = Button(opening_frame, command=closeprogram, text="start", padx=16,
+                              pady=12, bg="#7CDF64", fg="#131117", font=("Arial", 35))
+        start_button.place(x=300, y=680)
+
+        info_button = Button(opening_frame, command=lambda: open_window("info screen", window), text="info",
+                             padx=16, pady=12, bg="#E8C04A", fg="#131117", font=("Arial", 35))
+        info_button.place(x=900, y=680)
+
+        intro_text = Label(opening_frame, text="Drama Preperation Program", font=(
+            "Arial", 50), bg="#E2B6CE", fg="#131117")
+        intro_text.place(x=400, y=225)
+
+    elif new_window == "info screen":
+        window = new_window
+
+        info_frame = LabelFrame(
+            program, width=1720, height=900, bg="#E2B6CE")
+        info_frame.pack(anchor="nw")
+        info_text = Label(info_frame, text="Information", font=(
+            "Arial", 40), bg="#E2B6CE", fg="#131117")
+        info_text.place(x=0, y=0)
+        info_text.place(x=50, y=50)
+
+        info_back_button = Button(program, command=lambda: open_window(
+            "opening window", window), text="back", padx=10, pady=2, bg="#F45866", fg="#131117", font=("Arial", 25))
+        info_back_button.place(x=1500, y=40)
+
+        info_information1 = Label(info_frame, text="""Created by:
+                                    Jack Farrell""", font=(
+            "Arial", 20), bg="#E2B6CE", fg="#131117")
+        info_information1.place(x=1250, y=800)
+
+        info_information2 = Label(info_frame, text="This program is designed to help users to prepare for drama performances", font=(
+            "Arial", 30), bg="#E2B6CE", fg="#131117")
+        info_information2.place(x=60, y=300)
+
+        info_information3 = Label(info_frame, text="where they need to be in character. To start the program, return to the prevoius window", font=(
+            "Arial", 30), bg="#E2B6CE", fg="#131117")
+        info_information3.place(x=60, y=350)
+
+        info_information4 = Label(info_frame, text="and press START.", font=(
+            "Arial", 30), bg="#E2B6CE", fg="#131117")
+        info_information4.place(x=60, y=400)
+
     if old_window == "opening window":
         opening_frame.destroy()
     elif old_window == "info screen":
         info_frame.destroy()
-
-
-    if new_window == "opening window":
-        window = new_window
-        opening_frame = tk.LabelFrame(program, width=1720, height=900, bg="#E2B6CE")
-        opening_frame.pack(anchor="nw")
-        start_button = tk.Button(opening_frame, command=lambda:closeprogram(), text="start", padx=16, pady=12, bg="#7CDF64", fg="#131117", font=("Arial", 35)).place(x=550, y=680)
-        info_button = tk.Button(opening_frame, command=lambda:open_window("info screen", window), text="info", padx=16, pady=12, bg="#E8C04A", fg="#131117", font=("Arial", 35)).place(x=900, y=680)
-        intro_text = tk.Label(opening_frame, text="Drama Preperation Program", font=("Arial", 50), bg="#E2B6CE", fg="#131117").place(x=400, y=225)
-    elif new_window == "info screen":
-        window = new_window
-        info_frame = tk.LabelFrame(program, width=1720, height=900, bg="#E2B6CE")
-        info_frame.pack(anchor="nw")
-        info_text = tk.Label(info_frame, text="Information", font=("Arial", 40), bg="#E2B6CE", fg="#131117").place(x=50, y=50)
-        info_back_button = tk.Button(program, command=lambda:open_window("opening window", window), text="back", padx=10, pady=2, bg="#F45866", fg="#131117", font=("Arial", 25)).place(x=100, y=100)
 
 
 open_window("opening window", "")
