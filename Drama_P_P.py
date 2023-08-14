59f18d704bc096ea9ad1d8430e7d72ed41047449
Jack Farrell <farrellj@stu.otc.school.nz>
2023-08-14 15:19:58 +1200

    Changed the name of the main file, added more items to the prep list
    This will make the program more intuitive to open, and also will give the program more re-usability

--- Drama_P_P.py
+++ Drama_P_P.py
@@ -14,7 +14,7 @@ from tkinter import Label, LabelFrame, Button, Tk, messagebox
 # purple #724E91
 # yellow #E8C04A
 
-# pylint: disable = E1111, E0601
+# pylint: disable = E1111, E0601, W0601, W0621, C0103
 
 program = Tk()
 program.attributes('-fullscreen', True)
@@ -25,9 +25,13 @@ window = "opening window"
 activity_duration = ["please select a length of time",
                      "please select a difficulty"]
 activity_duration_options = [
-    ["short", "mid-length", "long", "easy", "medium", "hard"], ["#7CDF64", "#E8C04A", "#FF7F51", "green", "orange", "red"], [[140, 140], [100, 260], [140, 380], [340, 140], [320, 260], [340, 380]]]
+    ["short", "mid-length", "long", "easy", "medium", "hard"],
+    ["#7CDF64", "#E8C04A", "#FF7F51", "green", "orange", "red"],
+    [[140, 140], [100, 260], [140, 380], [340, 140], [320, 260], [340, 380]]]
 
-activity_options = ["cut lines", "who is your character", ""]
+activity_options = ["cut lines", "who is your character",
+                    "emotion memory recall", "calming circle", "info/tips"]
+calming_circle_options = ["colour breathing", "who is watching", ""]
 
 
 def closeprogram():
@@ -61,12 +65,14 @@ def open_window(new_window, old_window):
             program, width=1720, height=900, bg="#E2B6CE")
         opening_frame.pack(anchor="nw")
 
-        start_button = Button(opening_frame, command=lambda: open_window("start screen", window), text="start", padx=16,
-                              pady=12, bg="#7CDF64", fg="#131117", font=("Arial", 35))
+        start_button = Button(opening_frame, command=lambda: open_window("start screen", window),
+                              text="start", padx=16, pady=12,
+                              bg="#7CDF64", fg="#131117", font=("Arial", 35))
         start_button.place(x=550, y=680)
 
-        info_button = Button(opening_frame, command=lambda: open_window("info screen", window), text="info",
-                             padx=16, pady=12, bg="#E8C04A", fg="#131117", font=("Arial", 35))
+        info_button = Button(opening_frame, command=lambda: open_window("info screen", window),
+                             text="info", padx=16, pady=12,
+                             bg="#E8C04A", fg="#131117", font=("Arial", 35))
         info_button.place(x=1000, y=680)
 
         intro_text = Label(opening_frame, text="Drama Preperation Program", font=(
@@ -85,7 +91,8 @@ def open_window(new_window, old_window):
         info_text.place(x=50, y=50)
 
         info_back_button = Button(program, command=lambda: open_window(
-            "opening window", window), text="back", padx=10, pady=2, bg="#F45866", fg="#131117", font=("Arial", 25))
+            "opening window", window),
+            text="back", padx=10, pady=2, bg="#F45866", fg="#131117", font=("Arial", 25))
         info_back_button.place(x=1500, y=40)
 
         info_information1 = Label(info_frame, text="""Created by:
@@ -93,11 +100,14 @@ def open_window(new_window, old_window):
             "Arial", 20), bg="#E2B6CE", fg="#131117")
         info_information1.place(x=1250, y=800)
 
-        info_information2 = Label(info_frame, text="This program is designed to help users to prepare for drama performances", font=(
-            "Arial", 30), bg="#E2B6CE", fg="#131117")
+        info_information2 = Label(info_frame,
+                                  text="This program is designed to help users \
+to prepare for drama performances",
+                                  font=("Arial", 30), bg="#E2B6CE", fg="#131117")
         info_information2.place(x=60, y=300)
 
-        info_information3 = Label(info_frame, text="where they need to be in character. To start the program, return to the prevoius window", font=(
+        info_information3 = Label(info_frame, text="where they need to be in \
+character. To start the program, return to the prevoius window", font=(
             "Arial", 30), bg="#E2B6CE", fg="#131117")
         info_information3.place(x=60, y=350)
 
@@ -113,7 +123,9 @@ def open_window(new_window, old_window):
         for i in range(len(activity_duration_options[0])):
             activitybutton = Button(
                 start_frame, text=activity_duration_options[0][i], padx=16,
-                pady=12, bg=activity_duration_options[1][i], fg="#131117", font=("Arial", 25), command=lambda i=i: select_activity(i, window))
+                pady=12, bg=activity_duration_options[1][i],
+                fg="#131117", font=("Arial", 25),
+                command=lambda i=i: select_activity(i, window, activity_duration))
             activitybutton.place(
                 x=activity_duration_options[2][i][0], y=activity_duration_options[2][i][1])
 
@@ -124,11 +136,13 @@ def open_window(new_window, old_window):
         testlabel.place(x=60, y=60)
 
         activitystart_back_button = Button(start_frame, command=lambda: open_window(
-            "opening window", window), text="back", padx=10, pady=2, bg="#F45866", fg="#131117", font=("Arial", 25))
+            "opening window", window), text="back",
+            padx=10, pady=2, bg="#F45866", fg="#131117", font=("Arial", 25))
         activitystart_back_button.place(x=1500, y=40)
 
         continue_to_rep_button = Button(
-            start_frame, text="start prep", command=lambda: check_activity(activity_duration, window))
+            start_frame, text="start prep",
+            command=lambda: check_activity(activity_duration, window))
         continue_to_rep_button.place(x=600, y=400)
 
     elif new_window == "activity window":
@@ -138,10 +152,8 @@ def open_window(new_window, old_window):
         activity_frame.pack(anchor="nw")
 
 
-def select_activity(i, window):
+def select_activity(i, window, activity_duration):
     """These butons allow the user to choose how difficult and long they want their prep to be"""
-    global activity_duration
-
     if i == 0:
         activity_duration[0] = "short"
     elif i == 1:
@@ -160,7 +172,9 @@ def select_activity(i, window):
 
 def check_activity(selected_activities, window):
     """checks for both activity settings to have a selection and gives error message to user"""
-    if selected_activities[0] == "please select a length of time" or selected_activities[1] == "please select a difficulty":
+    if selected_activities[0] == "please select a \
+length of time" or selected_activities[1] == "please \
+select a difficulty":
         messagebox.showerror(
             title=None, message="please select a prep duration and difficulty")
     else:
